62c5822e74317f23e0dd418100ba163d1558ba5a
khoirulbads <irulad151@gmail.com>
2021-07-06 23:27:51 +0700

    grafik

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -84,5 +84,151 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div>
+            <canvas id="charttransaksi" style="height:350px;" height="200"></canvas>
+        </div>
+    </div>
+    <div class="row">
+        <div>
+            <canvas id="chartkategori" style="height:350px;" height="200"></canvas>
+        </div>
+    </div>
+    <div class="row">
+        <div>
+            <canvas id="chartsupplier" style="height:350px;" height="200"></canvas>
+        </div>
+    </div>
 </div>
+@php
+$transaksi = DB::select("select b.total, a.name from  users a, cart b where a.id=b.user_id order by total DESC limit 10");
+$kategori = DB::select("select count(a.id) as jumlah, b.nama_kategori from produk a, kategori b where a.kategori_id=b.id group by a.kategori_id");
+$supplier = DB::select("select count(a.id) as jumlah, b.nama from produk a, suppliers b where a.supplier_id=b.id group by a.supplier_id");
+
+@endphp
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+<script>
+    var user = [<?php foreach ($transaksi as $key) { ?>
+            '<?php echo $key->name ?>',
+        <?php }?>];
+    var total = [<?php foreach ($transaksi as $key) { ?>
+            '<?php echo $key->total ?>',
+        <?php }?>];
+    var barChartTransaksi = {
+        labels: user,
+        datasets: [{
+            label: 'Total Pembelian (Rupiah)',
+            backgroundColor: "pink",
+            data: total
+        }]
+    };
+
+    var kategori = [<?php foreach ($kategori as $key) { ?>
+            '<?php echo $key->nama_kategori ?>',
+        <?php }?>];
+    var jumlah = [<?php foreach ($kategori as $key) { ?>
+            '<?php echo $key->jumlah ?>',
+        <?php }?>];
+    var barChartKategori = {
+        labels: kategori,
+        datasets: [{
+            label: 'Jumlah Produk',
+            backgroundColor: "lightblue",
+            data: jumlah
+        }]
+    };
+
+    var supplier = [<?php foreach ($supplier as $key) { ?>
+            '<?php echo $key->nama ?>',
+        <?php }?>];
+    var jmlproduk = [<?php foreach ($supplier as $key) { ?>
+            '<?php echo $key->jumlah ?>',
+        <?php }?>];
+    var barChartSupplier = {
+        labels: supplier,
+        datasets: [{
+            label: 'Jumlah Produk',
+            backgroundColor: "grey",
+            data: jmlproduk
+        }]
+    };
+
+
+    window.onload = function() {
+        var chart1 = document.getElementById("charttransaksi").getContext("2d");
+        window.myBarTransaksi = new Chart(chart1, {
+            type: 'bar',
+            data: barChartTransaksi,
+            options: {
+                elements: {
+                    rectangle: {
+                        borderWidth: 2,
+                        borderColor: '#c1c1c1',
+                        borderSkipped: 'bottom'
+                    }
+                },
+                responsive: true,
+                title: {
+                    display: true,
+                },scales: {
+                    yAxes: [{
+                        ticks: {
+                        beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+        var chart2 = document.getElementById("chartkategori").getContext("2d");
+        window.myBarKategori = new Chart(chart2, {
+            type: 'bar',
+            data: barChartKategori,
+            options: {
+                elements: {
+                    rectangle: {
+                        borderWidth: 2,
+                        borderColor: '#c1c1c1',
+                        borderSkipped: 'bottom'
+                    }
+                },
+                responsive: true,
+                title: {
+                    display: true,
+                },scales: {
+                    yAxes: [{
+                        ticks: {
+                        beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+        var chart3 = document.getElementById("chartsupplier").getContext("2d");
+        window.myBarSupplier = new Chart(chart3, {
+            type: 'bar',
+            data: barChartSupplier,
+            options: {
+                elements: {
+                    rectangle: {
+                        borderWidth: 2,
+                        borderColor: '#c1c1c1',
+                        borderSkipped: 'bottom'
+                    }
+                },
+                responsive: true,
+                title: {
+                    display: true,
+                },scales: {
+                    yAxes: [{
+                        ticks: {
+                        beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+        
+    };
+</script>
+
 @endsection
